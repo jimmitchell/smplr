4514e58187d8ec83254033fb37b0bb44fe0d6375
Jim Mitchell <jim@jimmitchell.org>
2025-03-21 23:03:53 -0700

    2025-03-21

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navLink = document.querySelectorAll(".nav-link");
+const siteTitle = document.querySelectorAll(".site-title");
 
 hamburger.addEventListener("click", mobileMenu);
 navLink.forEach(n => n.addEventListener("click", closeMenu));
@@ -8,9 +9,11 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 function mobileMenu() {
 	hamburger.classList.toggle("active");
 	navMenu.classList.toggle("active");
+	siteTitle.classList.toggle("active");
 }
 
 function closeMenu() {
 	hamburger.classList.remove("active");
 	navMenu.classList.remove("active");
+	siteTitle.classList.remove("active");
 }
